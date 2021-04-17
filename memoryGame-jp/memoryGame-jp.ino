fe4700f39e806207bdea8795e1a3eb0400eeecdb
The Hurt Doctor <51414459+DoctorHurt@users.noreply.github.com>
2021-04-17 03:55:26 -0600

    added 7-segment LED display and RGB LED eyes

--- memoryGame-jp/memoryGame-jp.ino
+++ memoryGame-jp/memoryGame-jp.ino
@@ -2,6 +2,13 @@
 #include <Servo.h>
 #include "pitches.h"
 #define PLAYER_WAIT_TIME 3000 // The time allowed between button presses - 2s
+// Define RGB LED Pins
+#define BLUE 8
+#define GREEN 7
+#define RED 6
+#define BLUE2 12
+#define GREEN2 10
+#define RED2 9
 LiquidCrystal lcd(42, 44, 46, 48, 50, 52); // initialize the library with the numbers of the interface pins
 Servo myservo;
 byte sequence[100];           // Storage for the light sequence
@@ -17,6 +24,19 @@ byte soundPin = 11;            // Speaker output
 byte noPins = 5;              // Number of buttons/LEDs (While working on this, I was using only 2 LEDs)
 byte pins[] = {22, 24, 26, 28, 30}; // Button input pins and LED ouput pins - change these vaules if you want to connect your buttons to other pins                              
 long inputTime = 0;           // Timer variable for the delay between user inputs
+bool candy = true;
+int data = 3;//7-segment LED display data pin
+int latch = 4;//7-segment LED display latch pin
+int clock = 5;//7-segment LED display clock pin
+// define RGB variables
+int redValue;
+int greenValue;
+int blueValue;
+
+// This is for the 7-segment LED display
+unsigned char table[]=
+{0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c
+,0x39,0x5e,0x79,0x71,0x00};
 
 // notes in the melody:
 int melody[] = {
@@ -29,10 +49,27 @@ int noteDurations[] = {
 };
 
 void setup() {
+  pinMode(latch,OUTPUT);
+  pinMode(clock,OUTPUT);
+  pinMode(data,OUTPUT);
   delay(1000);                // This is to give me time to breathe after connection the arduino - can be removed if you want
   //Serial.begin(9600);         // Start Serial monitor. This can be removed too as long as you remove all references to Serial below
   lcd.begin(16, 2);// set up the LCD's number of columns and rows:
   myservo.attach(2);// Pin connected to the swervo
+  // Setup RGB LEDs
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, HIGH);
+  pinMode(RED2, OUTPUT);
+  pinMode(GREEN2, OUTPUT);
+  pinMode(BLUE2, OUTPUT);
+  digitalWrite(RED2, LOW);
+  digitalWrite(GREEN2, LOW);
+  digitalWrite(BLUE2, HIGH);
+  //Run the reset thingy right off the bat
   Reset();
 }
 
@@ -43,6 +80,16 @@ void setPinDirection(byte dir){
   for(byte i = 0; i < noPins; i++){
     pinMode(pins[i], dir); 
   }
+}
+
+//Display rounds that have been won
+void Display(unsigned char num)
+{
+
+  digitalWrite(latch,LOW);
+  shiftOut(data,clock,MSBFIRST,table[num]);
+  digitalWrite(latch,HIGH);
+  
 }
 
 //send the same value to all the LED pins
@@ -69,9 +116,18 @@ void flash(short freq){
   setPinDirection(OUTPUT); /// We're activating the LEDS now
   for(int i = 0; i < 5; i++){
     lcd.print("*");
+    // Also flash RGB LEDs red
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
+    digitalWrite(RED2, HIGH);
+    digitalWrite(GREEN2, LOW);
+    digitalWrite(BLUE2, LOW);
     writeAllPins(HIGH);
     beep(50);
     delay(freq);
+    digitalWrite(RED, LOW);
+    digitalWrite(RED2, LOW);
     writeAllPins(LOW);
     delay(freq);
   }
@@ -81,9 +137,17 @@ void flash(short freq){
 ///This function resets all the game variables to their default values
 ///
 void Reset(){
+  Display(0);
   lcd.clear();
   lcd.print("GET READY!!");// Print a message to the LCD.
   myservo.write(90);// move servos to center position -> 90°
+  //Reset RGB LEDs back to blue
+  digitalWrite(RED, LOW);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, HIGH);
+  digitalWrite(RED2, LOW);
+  digitalWrite(GREEN2, LOW);
+  digitalWrite(BLUE2, HIGH);
   flash(500);
   curLen = 0;
   inputCount = 0;
@@ -143,8 +207,14 @@ void loop() {
                             //****************//
     lcd.clear();
     lcd.print("PAY ATTENTION!");
+    // Change RGB led to red
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, LOW);
+    digitalWrite(RED2, HIGH);
+    digitalWrite(GREEN2, LOW);
+    digitalWrite(BLUE2, LOW);
     setPinDirection(OUTPUT);                      // We're using the LEDs
-    
     randomSeed(analogRead(A0));                   // https://www.arduino.cc/en/Reference/RandomSeed
     sequence[curLen] = pins[random(0,noPins)];    // Put a new random value in the next position in the sequence -  https://www.arduino.cc/en/Reference/random
     curLen++;                                     // Set the new Current length of the sequence
@@ -164,6 +234,14 @@ void loop() {
                             //***************//
                             // Player's turn //
                             //***************//
+    //Turn RGB LED green
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(BLUE, LOW);
+    digitalWrite(RED2, LOW);
+    digitalWrite(GREEN2, HIGH);
+    digitalWrite(BLUE2, LOW);
+
     setPinDirection(INPUT);                       // We're using the buttons
 
     if(millis() - inputTime > PLAYER_WAIT_TIME){  // If the player takes more than the allowed time,
@@ -208,6 +286,7 @@ void loop() {
           if(inputCount == curLen){                 // Has the player finished repeating the sequence
             wait = false;                           // If so, this will make the next turn the program's turn
             inputCount = 0;                         // Reset the number of times that the player has pressed a button
+            Display(char(curLen));
             delay(1500);
             if(curLen == winLen){
                 curLen++;
@@ -236,14 +315,80 @@ void loop() {
       // stop the tone playing:
       noTone(11);
     }
-    // Make swervo move like crazy.
-    myservo.write(90);// move servos to center position -> 90°
-    delay(500);
-    myservo.write(30);// move servos to center position -> 60°
-    delay(500);
-    myservo.write(90);// move servos to center position -> 90°
-    delay(500);
-    myservo.write(150);// move servos to center position -> 120°
+    // RGB LED stuff start
+    #define delayTime 1 // fading time between colors
+    
+    redValue = 255; // choose a value between 1 and 255 to change the color.
+    greenValue = 0;
+    blueValue = 0;
+    
+    // this is unnecessary as we've either turned on RED in SETUP
+    // or in the previous loop ... regardless, this turns RED off
+    // analogWrite(RED, 0);
+    // delay(1000);
+    
+    for(int i = 0; i < 255; i += 1) // fades out red bring green full when i=255
+    {
+    redValue -= 1;
+    greenValue += 1;
+    // The following was reversed, counting in the wrong directions
+    // analogWrite(RED, 255 - redValue);
+    // analogWrite(GREEN, 255 - greenValue);
+    analogWrite(RED, redValue);
+    analogWrite(GREEN, greenValue);
+    analogWrite(RED2, redValue);
+    analogWrite(GREEN2, greenValue);
+    delay(delayTime);
+    }
+    
+    redValue = 0;
+    greenValue = 255;
+    blueValue = 0;
+    
+    for(int i = 0; i < 255; i += 1) // fades out green bring blue full when i=255
+    {
+    greenValue -= 1;
+    blueValue += 1;
+    // The following was reversed, counting in the wrong directions
+    // analogWrite(GREEN, 255 - greenValue);
+    // analogWrite(BLUE, 255 - blueValue);
+    analogWrite(GREEN, greenValue);
+    analogWrite(BLUE, blueValue);
+    analogWrite(GREEN2, greenValue);
+    analogWrite(BLUE2, blueValue);
+    delay(delayTime);
+    }
+    
+    redValue = 0;
+    greenValue = 0;
+    blueValue = 255;
+    
+    for(int i = 0; i < 255; i += 1) // fades out blue bring red full when i=255
+    {
+    // The following code has been rearranged to match the other two similar sections
+    blueValue -= 1;
+    redValue += 1;
+    // The following was reversed, counting in the wrong directions
+    // analogWrite(BLUE, 255 - blueValue);
+    // analogWrite(RED, 255 - redValue);
+    analogWrite(BLUE, blueValue);
+    analogWrite(RED, redValue);
+    analogWrite(BLUE2, blueValue);
+    analogWrite(RED2, redValue);
+    delay(delayTime);
+    }
+    // RGB LED stuff end
 
+    // Make swervo move like crazy.
+    if(candy){
+      myservo.write(90);// move servos to center position -> 90°
+      delay(500);
+      myservo.write(30);// move servos to center position -> 60°
+      delay(500);
+      myservo.write(90);// move servos to center position -> 90°
+      delay(500);
+      myservo.write(150);// move servos to center position -> 120°
+      candy = false;
+    }
   }
 }
